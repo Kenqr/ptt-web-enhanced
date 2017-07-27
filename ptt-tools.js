d13773bcb20a3a6ac8a725207c7230d32a5715ca
Ken <ken75113@gmail.com>
2017-07-27 18:42:12 +0800

    高亮度顯示推文所有作者id

--- ptt-tools.js
+++ ptt-tools.js
@@ -1,3 +1,16 @@
+var posterUserid = null;
+
+//找出文章作者
+let articleMetalines = document.querySelectorAll('.article-metaline');
+for(let i=0; i<articleMetalines.length; i++) {
+    if (articleMetalines[i].querySelector('.article-meta-tag').innerHTML == '作者') {
+        let pattern = /\w+/;
+        let matches = articleMetalines[i].querySelector('.article-meta-value').innerHTML.match(pattern);
+        if(matches) posterUserid = matches[0];
+        break;
+    }
+}
+
 let pushUserids = document.querySelectorAll('.push-userid'); //所有推文id
 for(let i=0; i<pushUserids.length; i++) {
     //幫所有推文id加上class，方便後續select
@@ -18,4 +31,12 @@ for(let i=0; i<pushUserids.length; i++) {
             userids[j].classList.add('highlight-selected-userid');
         }
     });
+}
+
+//幫推文的文章作者id加上高亮度
+if (posterUserid) {
+    let userids = document.querySelectorAll('.userid-'+posterUserid);
+    for(let i=0; i<userids.length; i++) {
+        userids[i].classList.add('highlight-poster-userid');
+    }
 }
