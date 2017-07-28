e60119b23fb0fce32749b1d09eddf419a2420f3d
Ken <ken75113@gmail.com>
2017-07-28 12:06:27 +0800

    refactor ptt-tools.js

--- ptt-tools.js
+++ ptt-tools.js
@@ -1,42 +1,48 @@
-var posterUserid = null;
+let posterUserid = null; //文章作者id
+
+//querySelectorAll
+const $qsa = function(selector, baseElement = document){
+    return [...baseElement.querySelectorAll(selector)];
+};
+
+//querySelector
+const $qs = function(selector, baseElement = document){
+    return baseElement.querySelector(selector);
+};
 
 //找出文章作者
-let articleMetalines = document.querySelectorAll('.article-metaline');
-for(let i=0; i<articleMetalines.length; i++) {
-    if (articleMetalines[i].querySelector('.article-meta-tag').innerHTML == '作者') {
+$qsa('.article-metaline').forEach(articleMetaline => {
+    if ($qs('.article-meta-tag', articleMetaline).innerHTML == '作者') {
         let pattern = /\w+/;
-        let matches = articleMetalines[i].querySelector('.article-meta-value').innerHTML.match(pattern);
+        let matches = $qs('.article-meta-value', articleMetaline).innerHTML.match(pattern);
         if(matches) posterUserid = matches[0];
-        break;
     }
-}
+});
 
-let pushUserids = document.querySelectorAll('.push-userid'); //所有推文id
-for(let i=0; i<pushUserids.length; i++) {
+//對所有推文id欄位進行處理
+$qsa('.push-userid').forEach(pushUserid => {
+    let userid = pushUserid.innerHTML; //此則推文id
+
     //幫所有推文id加上class，方便後續select
-    pushUserids[i].classList.add('userid-'+pushUserids[i].innerHTML);
+    pushUserid.classList.add('userid-'+userid);
 
     //點選id時，將推文相同id高亮度顯示
-    pushUserids[i].addEventListener('click', function(){
+    pushUserid.addEventListener('click', function(){
         //移除原有highlight
-        let highlights = document.querySelectorAll('.highlight-selected-userid');
-        for(let j=0; j<highlights.length; j++) {
-            highlights[j].classList.remove('highlight-selected-userid');
-        }
+        $qsa('.highlight-selected-userid').forEach(highlight => {
+            highlight.classList.remove('highlight-selected-userid');
+        });
 
         //新增highlight
-        let userid = pushUserids[i].innerHTML;
-        let userids = document.querySelectorAll('.userid-'+userid);
-        for(let j=0; j<userids.length; j++) {
-            userids[j].classList.add('highlight-selected-userid');
-        }
+        $qsa('.userid-'+userid).forEach(userid => {
+            userid.classList.add('highlight-selected-userid');
+        });
     });
-}
+});
 
 //幫推文的文章作者id加上高亮度
 if (posterUserid) {
-    let userids = document.querySelectorAll('.userid-'+posterUserid);
-    for(let i=0; i<userids.length; i++) {
-        userids[i].classList.add('highlight-poster-userid');
-    }
+    $qsa('.userid-'+posterUserid).forEach(userid => {
+        userid.classList.add('highlight-poster-userid');
+    });
 }
