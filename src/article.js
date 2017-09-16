752d03a64986b01253e0eadfc59f38c917ab52af
Danny Lin <danny0838@gmail.com>
2017-09-16 09:01:56 +0800

    套件產生或加入的 class 都加上 "pwe-" 字首以避免衝突

--- src/article.js
+++ src/article.js
@@ -21,7 +21,7 @@ const init = async function(){
 //顯示通知訊息
 const notify = (() => {
     const pweNotify = document.createElement('div');
-    pweNotify.classList.add('pwe-notify', 'hidden');
+    pweNotify.classList.add('pwe-notify', 'pwe-hidden');
     $qs('body').insertBefore(pweNotify, null);
     let notifyTimeoutId = undefined;
 
@@ -33,9 +33,9 @@ const notify = (() => {
         }
         //顯示訊息，並定時移除
         pweNotify.textContent = message;
-        pweNotify.classList.remove('hidden');
+        pweNotify.classList.remove('pwe-hidden');
         notifyTimeoutId = setTimeout(function(){
-            pweNotify.classList.add('hidden');
+            pweNotify.classList.add('pwe-hidden');
         }, 5000);
     };
 })();
@@ -44,7 +44,7 @@ const notify = (() => {
 const processPushUserid = function(){
     $qsa('.push-userid').forEach(pushUserid => {
         const userid = pushUserid.innerHTML.trim(); //此則推文id
-        pushUserid.classList.add('userid-'+userid);
+        pushUserid.classList.add('pwe-userid-'+userid);
     });
 };
 
@@ -52,8 +52,8 @@ const processPushUserid = function(){
 const showFloor = function(){
     $qsa('.push').forEach((push, index) => {
         const floor = document.createElement('span');
-        floor.classList.add('floor');
-        if((index+1) % 5 == 0) floor.classList.add('floor-multiple-5'); //5的倍數樓層
+        floor.classList.add('pwe-floor');
+        if((index+1) % 5 == 0) floor.classList.add('pwe-floor-multiple-5'); //5的倍數樓層
         const textnode = document.createTextNode(`${index+1}樓`);
         floor.appendChild(textnode);
         push.insertBefore(floor, push.childNodes[0]);
@@ -79,7 +79,7 @@ const countPushStatistics = function(){
 
     //在文章後面顯示推文統計結果
     const pushStatistics = document.createElement('div');
-    pushStatistics.classList.add('push-statistics');
+    pushStatistics.classList.add('pwe-push-statistics');
     pushStatistics.textContent = `推噓文統計：推=${pushCount.good}, 噓=${pushCount.bad}, →=${pushCount.normal}`;
     $qs('#main-container').insertBefore(pushStatistics, $qs('#article-polling'));
 };
@@ -93,8 +93,8 @@ const clickToHighlightUserid = function(){
 
         pushUserid.addEventListener('click', function(){
             //移除原有highlight
-            $qsa('.highlight-selected-userid').forEach(highlight => {
-                highlight.classList.remove('highlight-selected-userid');
+            $qsa('.pwe-highlight-selected-userid').forEach(highlight => {
+                highlight.classList.remove('pwe-highlight-selected-userid');
             });
 
             //假如點選的id是目前選擇的id，就取消選取
@@ -104,8 +104,8 @@ const clickToHighlightUserid = function(){
             }
 
             //新增highlight
-            $qsa('.userid-'+userid).forEach(userid => {
-                userid.classList.add('highlight-selected-userid');
+            $qsa('.pwe-userid-'+userid).forEach(userid => {
+                userid.classList.add('pwe-highlight-selected-userid');
             });
             selectedUserid = userid; //更新目前選擇的id
         });
@@ -120,7 +120,7 @@ const resizeImage = function(){
         if(!img) return;
 
         //增加class，以套用CSS
-        richcontent.classList.add('richcontent-resize');
+        richcontent.classList.add('pwe-richcontent-resize');
     });
 };
 
@@ -128,7 +128,7 @@ const resizeImage = function(){
 const clickToDownloadImage = function(){
     //對所有圖片進行處理
     $qsa('.richcontent img').forEach(img => {
-        img.classList.add('clickable'); //指到圖片時滑鼠指標變成手
+        img.classList.add('pwe-clickable'); //指到圖片時滑鼠指標變成手
 
         //點選圖片時進行下載
         img.addEventListener('click', function(){
@@ -145,8 +145,8 @@ const clickToDownloadImage = function(){
 
 /* 頂部和底部導覽列自動隱藏 */
 const navbarAutohide = function(){
-    $qs('#topbar-container').classList.add('autohide');
-    $qs('#navigation-container').classList.add('autohide');
+    $qs('#topbar-container').classList.add('pwe-autohide');
+    $qs('#navigation-container').classList.add('pwe-autohide');
 };
 
 //高亮度文章作者id
@@ -165,7 +165,7 @@ const highlightPosterUserid = function(){
     //幫推文的文章作者id加上高亮度
     if (posterUserid) {
         $qsa('.userid-'+posterUserid).forEach(userid => {
-            userid.classList.add('highlight-poster-userid');
+            userid.classList.add('pwe-highlight-poster-userid');
         });
     }
 };
