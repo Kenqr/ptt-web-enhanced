f1315e04e65219b761edd0ebb4d89be057fc12ab
Ken <ken75113@gmail.com>
2017-09-23 15:10:23 +0800

    因為Fx52不支援sync storage，先改回使用local storage
    This reverts commit ff77266ccdcec587a44a2635a70d248af9f1dd05.

--- src/utils/settings.js
+++ src/utils/settings.js
@@ -11,21 +11,21 @@ pweSettings.defaultSettings = {
 };
 
 pweSettings.get = async function(key) {
-    const settings = await browser.storage.sync.get(key);
+    const settings = await browser.storage.local.get(key);
     return (settings[key] !== undefined) ? settings[key] : defaultSettings[key];
 };
 
 pweSettings.getAll = async function() {
-    const settings = await browser.storage.sync.get();
+    const settings = await browser.storage.local.get();
     return Object.assign({}, pweSettings.defaultSettings, settings);
 };
 
 pweSettings.set = async function(key, value) {
-    return browser.storage.sync.set({
+    return browser.storage.local.set({
         [key]: value
     });
 };
 
 pweSettings.reset = async function() {
-    return browser.storage.sync.clear();
+    return browser.storage.local.clear();
 };
