3c4140c093d079810e1d0b06bcac18403691f359
Ken <ken75113@gmail.com>
2017-09-25 18:04:13 +0800

    還原選項到預設值的按鈕不再需要確認
    Fx57 在選項頁面無法使用 confirm，因此改為不需要確認

--- src/options.js
+++ src/options.js
@@ -26,10 +26,8 @@ const loadSettings = async function(){
 
 //重置設定
 const resetSettings = async function(){
-    if(confirm('確定要將所有選項還原到預設值嗎？此動作無法復原！')) {
-        await pweSettings.reset();
-        loadSettings();
-    }
+    await pweSettings.reset();
+    loadSettings();
 };
 
 document.addEventListener('DOMContentLoaded', init);
