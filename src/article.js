46f6460f84720de1d8defa6c6857e04dd747b36f
Ken <ken75113@gmail.com>
2017-11-14 18:24:57 +0800

    推文 id 改用 data-userid 屬性紀錄，而非 class

--- src/article.js
+++ src/article.js
@@ -56,7 +56,7 @@ const notify = (() => {
 const processPushUserid = function(){
     $qsa('.push-userid').forEach(pushUserid => {
         const userid = pushUserid.innerHTML.trim(); //此則推文id
-        pushUserid.classList.add('pwe-userid-'+userid);
+        pushUserid.dataset.userid = userid;
     });
 };
 
@@ -247,7 +247,7 @@ const highlightPush = (function(){
         removeHl();
 
         //新增高亮度
-        $qsa('.pwe-userid-'+userid).forEach(userid => {
+        $qsa(`[data-userid="${userid}"]`).forEach(userid => {
             userid.parentElement.classList.add('pwe-highlight-push');
         });
 
@@ -524,7 +524,7 @@ const highlightPosterUserid = function(){
 
     //把文章作者的箭頭加上class
     if (posterUserid) {
-        $qsa('.pwe-userid-'+posterUserid).forEach(userid => {
+        $qsa(`[data-userid="${posterUserid}"]`).forEach(userid => {
             const pushTag = $qs('.push-tag', userid.parentElement);
             pushTag.classList.add('pwe-poster-push-tag');
         });
