d36a2091972db83a3ee0c09d5a2ae7afffeb680c
Danny Lin <danny0838@gmail.com>
2022-01-16 22:56:43 +0800

    取消 ALLPOST 連結修正
    - 新版的 PTT web 已無 ALLPOST 連結錯誤問題

--- src/board.js
+++ src/board.js
@@ -4,8 +4,6 @@ const init = async function(){
     $qs('html').classList.add('pwe');
 
     authorMenu();
-
-    if (pwe.boardName === 'ALLPOST') fixAllpostLink();
 };
 
 //幫作者ID加上搜尋選單
@@ -60,24 +58,6 @@ const authorMenu = function(){
                 ]
             ]
         ));
-    });
-};
-
-//修正 ALLPOST 板文章連結錯誤的問題
-const fixAllpostLink = function(){
-    $qsa('.r-ent').forEach(rEnt => {
-        //從文章列表的文章標題找出板名
-        const anchor = $qs('a', rEnt);
-        const re = /\(([\w-]+)\)$/; //match 板名
-        const result = re.exec(anchor.innerHTML);
-        if (!result[1]) {
-            console.error('偵測板名失敗!, result = ', result);
-            return;
-        }
-        const board = result[1];
-    
-        //將連結中的板名替換為正確的板名
-        anchor.href = anchor.href.replace('ALLPOST', board);
     });
 };
 
