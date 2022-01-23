923bd33edafb7ad99798b83ebf033f7bb17a4860
Danny Lin <danny0838@gmail.com>
2022-01-23 21:31:16 +0800

    改進初始化程式碼

--- src/article.js
+++ src/article.js
@@ -1,11 +1,10 @@
 const init = async function(){
-    await pweSettings.ready;
-
-    const settings = await pweSettings.getAll();
-
     // 套件重新啟用時重新載入頁面
     if ($qs('html.pwe')) { window.location.reload(); }
     $qs('html').classList.add('pwe');
+
+    await pweSettings.ready;
+    const settings = await pweSettings.getAll();
 
     showArticleTitle();
 
